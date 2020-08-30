8adb69ba7c5fd3263eb5cfa1570695d4afb29d39
Nathanael Ferreira Santana <nathanaelotaku28@gmail.com>
2020-08-30 12:36:13 -0300

    Projeto iniciado, camadas criadas, iniciando configuração de classe de mapeamento das models

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
---
+++ src/DevIO.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,48 @@
+﻿using AppMvcBasica.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Data.Mappings
+{
+    class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(p => p.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(1000)");
+
+            builder.Property(p => p.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.ToTable("Produtos");
+
+        }
+    }
+
+    class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+            
+
+            builder.ToTable("Fornecedores");
+
+        }
+    }
+}
